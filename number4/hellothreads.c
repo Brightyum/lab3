bdc3c7aec2282ba631a754bb52b32574968d565a
YeomSeungWook <duatmddnr@gmail.com>
2025-12-02 23:59:42 +0900

    number5

--- number4/hellothreads.c
+++ number4/hellothreads.c
@@ -3,23 +3,55 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-pthread_mutex mutex;
+pthread_mutex_t mutex;
+pthread_cond_t cond;
+int turn = 0; // 부모 차례 : 0, 자식 차례 : 1
 
-void *hello_thread(void *arg) {
-    printf("Hello from thread %d!\n", (int)(long)arg);
-    return arg;
+void *child_thread(void *arg) {
+    while(1) {
+        pthread_mutex_lock(&mutex);
+
+        while (turn != 1) {
+            pthread_cond_wait(&cond, &mutex);
+        }
+
+        printf("hello parent\n");
+        sleep(1);
+
+        turn = 0;
+        pthread_cond_signal(&cond);
+        
+        pthread_mutex_unlock(&mutex);
+    }
 }
 
 int main() {
-    pthread_t parent_tid, child_tid;
+    pthread_t child_tid;
     int status;
     void *result;
 
     pthread_mutex_init(&mutex, NULL);
 
-    status = pthread_create(&child_tid, NULL, hello_thread, (void *)(long)1);
+    status = pthread_create(&child_tid, NULL, child_thread, (void *)(long)1);
     if (status != 0) {
         fprintf(stderr, "create child thread: %d", status);
         exit(1);
     }
+
+    while (1) {
+        pthread_mutex_lock(&mutex);
+
+        while (turn != 0) {
+            pthread_cond_wait(&cond, &mutex);
+        }
+
+        printf("hello child\n");
+
+        turn = 1;
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&mutex);
+    }
+
+    pthread_join(child_tid, NULL);
+    return 0;
 }
